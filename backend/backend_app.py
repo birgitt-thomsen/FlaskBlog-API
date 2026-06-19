f33615e48691e7692b235eb0e8cb2ed4e9450599
Birgitt Thomsen <birgitt.thomsen@t-online.de>
2026-06-19 11:54:11 +0200

    Create DELETE route functionality

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -21,6 +21,15 @@ def validate_post_data(data):
     return missing_fields
 
 
+def find_post_by_id(post_id):
+    """ Find the post with the id `post_id`. If there is no post with this
+    id, return None."""
+    for post in POSTS:
+        if post["id"] == post_id:
+            return post
+    return None
+
+
 @app.route('/api/posts', methods=['GET'])
 def get_posts():
     """ Returns a list of all posts. """
@@ -52,6 +61,27 @@ def add_post():
     POSTS.append(new_post)
 
     return jsonify(new_post), 201
+
+
+@app.route('/api/posts/<int:post_id>', methods=['DELETE'])
+def delete_post(post_id):
+    """
+    Deletes the user selected post from the database and returns
+    success message. Returns error message when post id does not exist.
+    """
+    # Find the book with the given ID
+    post = find_post_by_id(post_id)
+
+    # If the book wasn't found, return a 404 error
+    if post is None:
+        return jsonify({"error": f"Post with id '{post_id}' not found"}), 404
+
+    # Remove the post from the list
+    POSTS.remove(post)
+
+    # Return success message
+    return (jsonify({"message": f"Post with id '{post_id}' has been "
+                                "successfully deleted."}),200)
 
 
 if __name__ == '__main__':
